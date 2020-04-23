80b44bc37ecd2fef1c4eb7545ae3dd143e9a6918
Your Name <you@example.com>
2020-04-23 22:52:28 +0900

    ログインの実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -14,3 +16,17 @@ def signupfunc(request):
             user = User.objects.create_user(username2, '', password2)
             return render(request, 'signup.html', {'some': 100})
     return render(request, 'signup.html', {'some': 100})
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
