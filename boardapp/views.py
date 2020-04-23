bf9eb98d556fc4c26f037e413e1042ffe83f51e3
Your Name <you@example.com>
2020-04-23 22:33:36 +0900

    ユーザ登録画面

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some': 100})
     return render(request, 'signup.html', {'some': 100})
