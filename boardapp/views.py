3221b3198c86fbe1d4aaf99ee4da755ba6c26639
takayan <takayan40@users.noreply.github.com>
2020-05-12 00:06:57 +0900

    個別ページの作成

--- boardapp/views.py
+++ boardapp/views.py
@@ -41,3 +41,7 @@ def listfunc(request):
 def logoutfunc(request):
     logout(request)
     return redirect('login')
+
+def detailfunc(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    return render(request, 'detail.html', {'object':object})
