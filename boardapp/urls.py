19efab3ca04f737215e392352a116bc21c5c3735
takayan <takayan40@users.noreply.github.com>
2020-05-12 00:19:29 +0900

    いいね機能を実装

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc
+from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc
 
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
     path('login/', loginfunc, name='login'),
     path('list/', listfunc, name='list'),
     path('logout/', logoutfunc, name='logout'),
-    path('detail/<int:pk>', detailfunc, name='detail')
+    path('detail/<int:pk>', detailfunc, name='detail'),
+    path('good/<int:pk>', goodfunc, name='good')
 ]
  
